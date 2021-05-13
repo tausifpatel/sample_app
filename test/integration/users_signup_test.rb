4d8a58f6dca20048f981fd1ee7eda42fb6dabe24
Tausif <tausifpatel.dev@gmail.com>
2021-05-13 13:50:11 +0100

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,39 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do 
+    get signup_path
+    assert_no_difference "User.count" do 
+      post users_path, params: { user: { name: "", 
+                                         email: "hello@",
+                                         password: "123",
+                                         password_confirmation: "456" }}
+    end 
+    assert_template 'users/new'
+  end 
+
+  test "valid signup information" do 
+    get signup_path
+    assert_difference "User.count", 1 do 
+      post users_path, params: { user: { name:  "Michael", 
+                                         email: "Michael@gmail.com",
+                                         password:              "123456",
+                                         password_confirmation: "123456" }}
+    end 
+    follow_redirect!
+    assert_template 'users/show'
+  end 
+
+  test "valid signup information shows alert message" do 
+    get signup_path
+    assert_difference "User.count", 1 do 
+      post users_path, params: { user: { name:  "Michael", 
+                                         email: "Michael@gmail.com",
+                                         password:              "123456",
+                                         password_confirmation: "123456" }}
+    end 
+    follow_redirect!
+    assert_template "users/show"
+    assert_not flash.empty? 
+  end 
+end
